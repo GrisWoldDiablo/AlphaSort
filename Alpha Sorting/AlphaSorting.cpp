97f041c74e671ae3052c600d6071e3debdc70802
GrisWold Diablo <GrisWoldDiablo@users.noreply.github.com>
2017-11-26 21:50:35 -0500

    New

---
+++ Alpha Sorting/AlphaSorting.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <random>
+
+
+
+void swapItem(std::string  &firstItem, std::string  &secondItem) {
+
+	std::string  temp = firstItem;
+	firstItem = secondItem;
+	secondItem = temp;
+
+}
+
+void sortString(std::string string[], int sizeOfString) {
+
+	for (int i = 1; i < sizeOfString; i++) {
+
+		int j = i;
+		while (j > 0 && (string[j - 1][0] >= string[j][0])) {
+
+			swapItem(string[j], string[j - 1]);
+			for (int k = 0; k < string[j - 1].length(); k++) {
+
+				if (string[j - 1][k] > string[j][k]) {
+
+					swapItem(string[j], string[j - 1]);
+					break;
+				}
+				else if (string[j - 1][k] != string[j][k]) {
+					break;
+				}
+			}
+			j--;
+		}
+	}
+}
+
+int main() {
+	const int arraySize = 20;
+	std::random_device rando;
+	std::string test[] = { " ","A","B" };
+	std::string strings[arraySize];
+	for (int i = 0; i < arraySize; i++) {
+		std::string namerandom;
+		for (int i = 0; i < 5; i++) {
+			namerandom += test[rand() % 3];
+		}
+		strings[i] += namerandom;
+	}
+
+	sortString(strings, arraySize);
+	for (int i = 0; i < arraySize; i++) {
+		std::cout << strings[i] << std::endl;
+	}
+	return 0;
+}
